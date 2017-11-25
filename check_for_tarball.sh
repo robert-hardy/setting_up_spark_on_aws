6201707cbb113e8e2f31da867a54fe40846dfd48
Robert Hardy <robbert@gmail.com>
2017-11-25 08:14:46 +0000

    Status check script working just fine.

--- check_for_tarball.sh
+++ check_for_tarball.sh
@@ -4,10 +4,18 @@ for host in "${hosts[@]}"
 do
 	echo Checking $host for the tarball
 	ssh ubuntu@$host test -e spark-2.2.0-bin-hadoop2.7.tgz
-	FILE_EXISTS=$?
-	echo $FILE_EXISTS
+	EXIT_CODE=$?
+	if [ $EXIT_CODE -eq 0 ]; then
+		echo The tarball is there.
+	else
+		echo The tarball is not there.
+	fi
 	echo Checking $host for the directory
 	ssh ubuntu@$host test -e /usr/local/spark
-	DIR_EXISTS=$?
-	echo $DIR_EXISTS
+	EXIT_CODE=$?
+	if [ $EXIT_CODE -eq 0 ]; then
+		echo The tarball has been unpacked.
+	else
+		echo The tarball has not been unpacked.
+	fi
 done
