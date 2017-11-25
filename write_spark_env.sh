d1d67c413410b04ce0f4aa8621b77af18d7b67de
Robert Hardy <robbert@gmail.com>
2017-11-25 18:50:49 +0000

    Change to use only 1 core.

--- write_spark_env.sh
+++ write_spark_env.sh
@@ -1,5 +1,5 @@
 cat << EOF | sudo tee -a /usr/local/spark/conf/spark-env.sh
 export JAVA_HOME=$(jrunscript -e 'java.lang.System.out.println(java.lang.System.getProperty("java.home"));')
 export SPARK_PUBLIC_DNS="`curl -s http://169.254.169.254/latest/meta-data/public-hostname`"
-export SPARK_WORKER_CORES=6
+export SPARK_WORKER_CORES=1
 EOF
