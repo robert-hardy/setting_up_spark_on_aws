67da4c869558c0a975238549c6386f88420dc5b9
Robert Hardy <robbert@gmail.com>
2017-11-25 08:08:18 +0000

    Add the wget.

--- myscript.sh
+++ myscript.sh
@@ -5,4 +5,6 @@ do
 	ssh ubuntu@$host sudo apt-get update
 	ssh ubuntu@$host sudo apt-get --assume-yes install openjdk-8-jdk
 	ssh ubuntu@$host sudo apt-get --assume-yes install scala
+	ssh ubuntu@$host wget http://apache.mirror.anlx.net/spark/spark-2.2.0/spark-2.2.0-bin-hadoop2.7.tgz
+	ssh ubuntu@$host sudo tar zxvf spark-2.2.0-bin-hadoop2.7.tgz -C /usr/local
 done
